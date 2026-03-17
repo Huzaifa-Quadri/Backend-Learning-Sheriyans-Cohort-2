3427fad7c82c37cab554dbef23c846155f754b2d
Huzaifa Quadri <huzaifaquadri1853@gmail.com>
2026-03-17 15:13:25 +0530

    Deploying frontend at backend, both together on same port

--- PProject-2-Moodify/backend/src/app.js
+++ PProject-2-Moodify/backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const cookieparser = require("cookie-parser");
+app.use(express.static("./public")); //Make the mentioned folder publically available
+
 app.use(cookieparser());
 app.use(express.json());
 
@@ -9,4 +12,13 @@ const songRouter = require("./routes/song.routes");
 
 app.use("/api/auth", authRouter);
 app.use("/api/songs", songRouter);
+app.get("/api/test", (req, res) => {
+  res.status(200).json({
+    message: "Backend is running this is a TEST API",
+  });
+});
+app.get(/^(.*)$/, (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 module.exports = app;
